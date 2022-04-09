b33de5fc1e8c3d9d10308dec8abc7debbde8e4a9
tulasiram3786 <102223593+tulasiram3786@users.noreply.github.com>
2022-04-09 17:55:56 +0530

    Update CreateSampleTest.java

--- projectsample/src/test/java/samplepro/CreateSampleTest.java
+++ projectsample/src/test/java/samplepro/CreateSampleTest.java
@@ -10,16 +10,6 @@ public class CreateSampleTest {
 	   System.out.println("create test2 sucess");
 	   
 	   
-	   System.out.println("create test2 sucess");
-	   System.out.println("create test2 sucess");
-	     System.out.println("create test2 sucess");
-		   System.out.println("create test2 sucess");
-		   System.out.println("create test2 sucess");
-		   
-		   
-		   System.out.println("create test2 sucess");
-		   System.out.println("create test2 sucess");
-		     System.out.println("create test2 sucess");
 	   
 	     
 	     
