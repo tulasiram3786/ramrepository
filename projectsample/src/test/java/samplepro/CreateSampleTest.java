8801e55aa4da3026a07f6460b526e542e4f03654
chethanahj <chethana0016@gmail.com>
2022-04-09 18:23:02 +0530

    dsvdvfdsv

--- projectsample/src/test/java/samplepro/CreateSampleTest.java
+++ projectsample/src/test/java/samplepro/CreateSampleTest.java
@@ -4,9 +4,6 @@ public class CreateSampleTest {
 	
 	public void creteTest() {
 		System.out.println("create test sucess");
-	}
-   public void create2Test() {
-	   System.out.println("create test2 sucess");
 
 	   
 	   
